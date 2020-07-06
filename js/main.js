00f6c1e9a273fbcfa35b30ac9c1063c64e49feb8
tercsd <tercsd@gmail.com>
2020-07-06 17:00:29 +0200

    js test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("hello js");
+console.log("main.js start");
+function calcAmount() {
+    let price = 1000;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let showAmount = document.querySelector("span.show-amount");
+    let amount = parseInt(amountInput.value) * price;
+    
+    showAmount.innerHTML = amount;
+    console.log("amount calculated");
+}
+console.log("main.js end");
