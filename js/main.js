0744a4dd9584179589222258a3b5a0ca189af897
tercsd <tercsd@gmail.com>
2020-07-06 19:31:49 +0200

    ifNaN

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@ function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
     let showAmount = document.querySelector("span.show-amount");
-    let amountNumber = parseInt(amountInput.value)
-
+    let amountNumber = parseInt(amountInput.value);
+    if (isNaN(amountNumber)) {
+        amountNumber = 0;
+    }
     if (amountNumber > 10) {
         alert("max 10 vehető");
     } else if (amountNumber < 1) {
         alert("min 1 kell");
     } else {
-        let amount = parseInt(amountInput.value) * price;
+        let amount = amountNumber * price;
         showAmount.innerHTML = amount;
     }
 }
