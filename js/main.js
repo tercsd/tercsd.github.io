1c9aabe57c57e79de8e9df8144315e7eabdf7292
tercsd <tercsd@gmail.com>
2020-07-06 19:27:42 +0200

    if, if else, else

--- js/main.js
+++ js/main.js
@@ -3,7 +3,16 @@ function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
     let showAmount = document.querySelector("span.show-amount");
-    let amount = parseInt(amountInput.value) * price;
-    
-    showAmount.innerHTML = amount;
+    let amountNumber = parseInt(amountInput.value)
+
+    if (amountNumber > 10) {
+        alert("max 10 vehető");
+    } else if (amountNumber < 1) {
+        alert("min 1 kell");
+    } else {
+        let amount = parseInt(amountInput.value) * price;
+        showAmount.innerHTML = amount;
+    }
 }
+
+
