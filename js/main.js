0219520d913a193bc9dd9462d5a1ecfdca44bc65
tercsd <tercsd@gmail.com>
2020-07-14 17:36:17 +0200

    clean code

--- js/main.js
+++ js/main.js
@@ -2,9 +2,14 @@
 function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
-    let showAmount = document.querySelector("span.show-amount");
     let amountNumber = parseInt(amountInput.value);
     amountNumber = (isNaN(amountNumber)) ? 0 : amountNumber;
+    showSumPrice(price, amountNumber)
+}
+
+function showSumPrice(price, amountNumber){
+    amountNumber = amountNusmber || 0;
+    let showAmount = document.querySelector("span.show-amount");
     if (amountNumber > 10) {
         alert("max 10 vehető");
     } else if (amountNumber < 1) {
@@ -12,7 +17,6 @@ function calcAmount() {
     } else {
         let amount = amountNumber * price;
         showAmount.innerHTML = amount;
-    }
+    } 
 }
-
 
